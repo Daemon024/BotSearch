28f413ded5fc6723725ebe98909ac69c3bc87829
Florian Vidal <joy.roarr340@yahoo.fr>
2016-02-25 13:22:56 +0100

    Update

--- scrape.js
+++ scrape.js
@@ -2,6 +2,8 @@ var google = require('google');
 // var wait = require('wait.for');
 var request = require("request");
 var cheerio = require("cheerio");
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 // var hma = require('hma-proxy-scraper');
 var extractor = require('email-extractor').Extractor;
 var fs = require('fs');
@@ -9,9 +11,24 @@ var fs = require('fs');
 google.resultsPerPage = 100
 var nextCounter = 0
 var emailBis = "";
-//////////////////
-var argv = require('minimist')(process.argv.slice(1));
+// var url = 'mongodb://localhost:27017/mail';
 
+// //connect away
+// MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+//   if (err) throw err;
+//   console.log("Connected to Database");
+
+//   //simple json record
+//   var document = {name:"David", title:"About MongoDB"};
+  
+//   //insert record
+//   db.collection('test').insert(document, function(err, records) {
+//     if (err) throw err;
+//     console.log("Record added as "+records[0]._id);
+//   });
+// });
+
+//////////////////
 // Proxies for google /////////////////////////////////
 // var proxieslist = {};
 // var proxies = hma.getProxies(function (err,proxies) {
@@ -22,49 +39,63 @@ var argv = require('minimist')(process.argv.slice(1));
 ///////////////////////////////////////////////////////
 // orange();
 
-function orange (str) {
-// //console.log("Keyword in use: ", argv.k);
-// // var str = argv.k;
-// // str = str.replace(/\s+/g, '');
-// // console.log("File for save: ", str);
-// // fs.writeFile(str+'.txt', '');
+function free (str) {
+  console.log("Keyword in use: ", str);
+  console.log("-----------");
+  file = str.replace(/\s+/g, '');
+  console.log("File for save: ", file);
+  console.log("-----------");
+  fs.writeFile(file+'.txt', '');
+  //
   var tmp = ""
   var links = [];
   var emailB = "";
-  var ap = 1;
   str = str.replace(/\s+/g, '%20');
-  console.log(str);
-     for (var i = 0; i < 49; i++) {
-      console.log(i);
-        request({
-    uri: "http://lemoteur.orange.fr/?module=lemoteur&bhv=web_fr&kw="+str+"&profil=orange2&ap="+i,
-  }, function(error, response, body) {
-    var $ = cheerio.load(body);
-    $(".ellipsisLine").each(function() {
-      var link = $(this);
-      var text = link.text();
-      tmp = text;
-      tmp = tmp.replace(/(\r\t|\t|\r)/gm,"");
-      tmp = tmp.replace(/(\r\n|\n|\r)/gm,"");
-      tmp = "http://" + tmp;
-      links.push(tmp);
-    });
-    for (var i = 0; i < links.length; i++) {
-      console.log(links[i]);
-      extractor(links[i], function (url, email){
-        if (email !== emailB){
-          console.log("email: ", email);
-          emailB = email;
+  //
+    for (i = 0; i < 50; i++) {
+      request({   
+        uri: "http://search.free.fr/google.pl?page="+i+"&qs="+str,
+      }, function(error, response, body) {
+        var $ = cheerio.load(body);
+        $(".linkgoogle").each(function() {
+          var link = $(this);
+          var text = link.text();
+          tmp = text;
+          links.push(tmp);
+        });
+        for (var i = 0; i < links.length; i++) {
+          extractor(links[i], function (url, email){
+          var match = email.match(/(gif|png|jpg|jpeg)$/);
+            if (email !== emailB && match == null){
+              console.log(email);
+            var insertDocument = function(db, callback) {
+               db.collection('mail').insertOne( {
+                  "mail" : {
+                     "mail" : email,
+                     "url" : url,
+                  },
+               }, function(err, result) {
+                assert.equal(err, null);
+                console.log("Inserted a document into the local collection.");
+                callback();
+              });
+            };
+              // fs.appendFile(file+'.txt', email + "\n", function (err) {
+              //    if(err) {
+              //       return console.log(err);
+              //      }
+              //     console.log("The file was saved!");
+              // }); 
+              emailB = email;
+            }
+          });
         }
       });
     }
-    //console.log(links);
-  });
-    };
-
 }
 
-orange('Agence de communication Test');
+free('Developpement web agency website contact');
+
 /////////////////////////////////////////////////////////
 
 // console.log("Keyword in use: ", argv.k);
---
+++ scrape2.js
@@ -0,0 +1,128 @@
+var google = require('google');
+// var wait = require('wait.for');
+var request = require("request");
+var cheerio = require("cheerio");
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+// var hma = require('hma-proxy-scraper');
+var extractor = require('email-extractor').Extractor;
+var fs = require('fs');
+// Google Params //
+google.resultsPerPage = 100
+var nextCounter = 0
+var emailBis = "";
+// var url = 'mongodb://localhost:27017/mail';
+
+// //connect away
+// MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+//   if (err) throw err;
+//   console.log("Connected to Database");
+
+//   //simple json record
+//   var document = {name:"David", title:"About MongoDB"};
+  
+//   //insert record
+//   db.collection('test').insert(document, function(err, records) {
+//     if (err) throw err;
+//     console.log("Record added as "+records[0]._id);
+//   });
+// });
+
+//////////////////
+// Proxies for google /////////////////////////////////
+// var proxieslist = {};
+// var proxies = hma.getProxies(function (err,proxies) {
+//     if(err)
+//         throw err
+// });
+// console.log(proxies);
+///////////////////////////////////////////////////////
+// orange();
+
+function free (str) {
+  console.log("Keyword in use: ", str);
+  console.log("-----------");
+  file = str.replace(/\s+/g, '');
+  console.log("File for save: ", file);
+  console.log("-----------");
+  fs.writeFile(file+'.txt', '');
+  //
+  var tmp = ""
+  var links = [];
+  var emailB = "";
+  str = str.replace(/\s+/g, '%20');
+  //
+    for (i = 0; i < 50; i++) {
+      request({   
+        uri: "http://search.free.fr/google.pl?page="+i+"&qs="+str,
+      }, function(error, response, body) {
+        var $ = cheerio.load(body);
+        $(".linkgoogle").each(function() {
+          var link = $(this);
+          var text = link.text();
+          tmp = text;
+          links.push(tmp);
+        });
+        for (var i = 0; i < links.length; i++) {
+          extractor(links[i], function (url, email){
+          var match = email.match(/(gif|png|jpg|jpeg)$/);
+            if (email !== emailB && match == null){
+              console.log(email);
+              // fs.appendFile(file+'.txt', email + "\n", function (err) {
+              //    if(err) {
+              //       return console.log(err);
+              //      }
+              //     console.log("The file was saved!");
+              // }); 
+              emailB = email;
+            }
+          });
+        }
+      });
+    }
+}
+
+free('Solutions de développement applications mobiles');
+
+/////////////////////////////////////////////////////////
+
+// console.log("Keyword in use: ", argv.k);
+// var str = argv.k;
+// str = str.replace(/\s+/g, '');
+// console.log("File for save: ", str);
+// fs.writeFile(str+'.txt', '');
+// google(argv.k+' inurl:contact site:.fr', function (err, next, links){
+//  if (err) console.error(err)
+//  for (var i = 0; i < links.length; ++i) {
+//    console.log("link: ", links[i].link);
+//      extractor(links[i].link,function(url, email){
+//        var match = email.match(/(gif|png|jpg|jpeg)$/);
+//        if (email !== emailBis && match == null) {
+//          fs.appendFile(str+'.txt', email + "\n", function (err) {
+//        });
+//        emailBis = email;
+//      }
+//    });
+//  }
+//  if (nextCounter < 5) {
+//     nextCounter += 1
+//    if (next) next()
+//  }
+// })
+// (err) console.error(err)
+// 	for (var i = 0; i < links.length; ++i) {
+// 		console.log("link: ", links[i].link);
+// 	  	extractor(links[i].link,function(url, email){
+// 	  		var match = email.match(/(gif|png|jpg|jpeg)$/);
+// 	  		if (email !== emailBis && match == null) {
+// 			  	fs.appendFile(str+'.txt', email + "\n", function (err) {
+// 				});
+// 				emailBis = email;
+// 			}
+// 		});
+// 	}
+// 	if (nextCounter < 5) {
+//     nextCounter += 1
+// 		if (next) next()
+// 	}
+// })
